351b01bfd83d599af0a4ead15c7ecc94a01f5fa5
Ricardo Quadros <ricardaoquadros@gmail.com>
2025-10-03 14:44:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9883201907-44622",
+  "appId": "1:501991612233:web:8b717c0e2c8e2e40234498",
+  "apiKey": "AIzaSyA7CZgrJJV6pxe1cPxMgrdw5_G2K7jzkCM",
+  "authDomain": "studio-9883201907-44622.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "501991612233"
+};
